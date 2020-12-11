761988ea6444f42da0cd45f8aa82552bc5c0015f
Xiaolin Jin Lin <x140l1n@hotmail.com>
2020-12-11 03:22:46 +0100

    Avanzando animación

--- app/src/main/java/com/example/edujoc_cepsoft/MapaActivity.java
+++ app/src/main/java/com/example/edujoc_cepsoft/MapaActivity.java
@@ -1,19 +1,11 @@
 package com.example.edujoc_cepsoft;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-
-import com.example.edujoc_cepsoft.Data.Personaje;
-
-import java.io.File;
 
 public class MapaActivity extends MiActivityPersonalizado
 {
@@ -23,39 +15,50 @@ public class MapaActivity extends MiActivityPersonalizado
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
-        Personaje personaje = new Personaje(3, "Trump", "es", ".//personajes//img//trump_es_3.png","sdshdhsdjhsjd");
-        ImageView personajeMapa = findViewById(R.id.personajeMapa);
+        //Personaje personaje = new Personaje(3, "Trump", "es", ".//personajes//img//trump_es_3.png","sdshdhsdjhsjd");
+        //ImageView personajeMapa = findViewById(R.id.personajeMapa);
 
         //String rutaAbsolutaPersonaje = personaje.getRutaImagen();
         //Bitmap bitmapPersonaje = BitmapFactory.decodeFile(rutaAbsolutaPersonaje);
         //personajeMapa.setImageBitmap(bitmapPersonaje);
 
-        View view = findViewById(R.id.personaje);
+        final View personaje = findViewById(R.id.personaje);
 
-        AnimatorSet animatorSet = new AnimatorSet();
-        ObjectAnimator oa = ObjectAnimator.ofFloat(view, "translationX", 375.50f);
-        oa.setDuration(2000);
-        ObjectAnimator oa1 = ObjectAnimator.ofFloat(view, "translationY", -170f);
-        oa1.setDuration(2000);
-        ObjectAnimator oa2 = ObjectAnimator.ofFloat(view, "translationX",  (this.getResources().getDisplayMetrics().heightPixels - 1300));
-        oa2.setDuration(2000);
-        animatorSet.playSequentially(oa, oa1, oa2);
-        animatorSet.start();
+        final Button btnJugar = personaje.findViewById(R.id.btnJugar);
 
+        AnimatorSet as = new AnimatorSet();
 
+        as.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                btnJugar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                btnJugar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                System.err.println("Cancel");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                System.err.println("Start");
+            }
+        });
+
+        ObjectAnimator oa1 = ObjectAnimator.ofFloat(personaje, View.TRANSLATION_X, personaje.getX() + 350f);
+        ObjectAnimator oa2 = ObjectAnimator.ofFloat(personaje, View.TRANSLATION_Y, personaje.getY() - 150f);
+        ObjectAnimator oa3 = ObjectAnimator.ofFloat(personaje, View.TRANSLATION_X, personaje.getX() + 540f);
+        ObjectAnimator oa4 = ObjectAnimator.ofFloat(personaje, View.TRANSLATION_Y, personaje.getY() - 320f);
+        ObjectAnimator oa5 = ObjectAnimator.ofFloat(personaje, View.TRANSLATION_X, personaje.getX() + 85f);
+        ObjectAnimator oa6 = ObjectAnimator.ofFloat(personaje, View.TRANSLATION_Y, personaje.getY() - 420f);
+
+        as.playSequentially(oa1, oa2, oa3, oa4, oa5, oa6);
+        as.setDuration(1200);
+        as.start();
     }
-    /*Crear funcio de startActivityForResult
-
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        ;
-        ObjectAnimator oa = ObjectAnimator.ofFloat(view, "translationX", 375.50f);
-        oa.setDuration(2000);
-        ObjectAnimator oa1 = ObjectAnimator.ofFloat(view, "translationY", -170f);
-        oa1.setDuration(2000);
-        ObjectAnimator oa2 = ObjectAnimator.ofFloat(view, "translationX",  (this.getResources().getDisplayMetrics().heightPixels - 1300));
-        oa2.setDuration(2000);
-        animatorSet.playSequentially(oa, oa1, oa2);
-        animatorSet.start();
-     */
 }
