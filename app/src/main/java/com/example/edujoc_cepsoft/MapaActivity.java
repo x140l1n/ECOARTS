8ea4a3045ff00efe7376d5bef34167409886ece2
Oriol <urimipe@gmail.com>
2020-12-14 11:45:54 +0100

    fin batalla i creditos

--- app/src/main/java/com/example/edujoc_cepsoft/MapaActivity.java
+++ app/src/main/java/com/example/edujoc_cepsoft/MapaActivity.java
@@ -17,8 +17,17 @@ import androidx.annotation.Nullable;
 
 import com.example.edujoc_cepsoft.Data.Enemigo;
 import com.example.edujoc_cepsoft.Data.Personaje;
+import com.example.edujoc_cepsoft.Data.Pregunta;
+import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 
 public class MapaActivity extends MiActivityPersonalizado
 {
@@ -34,6 +43,7 @@ public class MapaActivity extends MiActivityPersonalizado
     private int numBatalla = 1;
 
     private ArrayList<Enemigo> enemigos;
+    private ArrayList<Pregunta> preguntas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -58,20 +68,27 @@ public class MapaActivity extends MiActivityPersonalizado
         personajeMapa.setImageBitmap(personaje.obtenerImagen(this));
 
         enemigos = cargarEnemigos();
-
+        preguntas = cargarPreguntas();
         btnJugar.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
                 Intent intent = new Intent(MapaActivity.this, BatallaActivity.class);
-
+                intent.putExtra(BatallaActivity.PREGUNTAS, preguntas);
                 intent.putExtra(BatallaActivity.ENEMIGO, enemigos.get(numBatalla - 1));
                 intent.putExtra(BatallaActivity.PERSONAJE, personaje);
                 intent.putExtra(BatallaActivity.NUMERO_BATALLA, numBatalla);
                 intent.putExtra(BatallaActivity.JUGADOR, nombreJugador.getText());
+                if(numBatalla < 6)
+                {
+                    startActivityForResult(intent, BatallaActivity.BATALLA_ACTIVITY);
+                }
+                else
+                {
+                    startActivity(intent);
+                }
 
-                startActivityForResult(intent, BatallaActivity.BATALLA_ACTIVITY);
             }
         });
 
@@ -146,8 +163,8 @@ public class MapaActivity extends MiActivityPersonalizado
             case BatallaActivity.BATALLA_ACTIVITY:
                 if (resultCode == RESULT_OK)
                 {
-                    Personaje personaje = (Personaje) data.getSerializableExtra(PERSONAJE);
-
+                    Personaje personaje = (Personaje) data.getSerializableExtra(BatallaActivity.PERSONAJE);
+                    preguntas = (ArrayList) data.getSerializableExtra(BatallaActivity.PREGUNTAS);
                     if (nivel.equals("facil")) personaje.setVidas(personaje.getVIDA_MAXIMA()); //Restablecer las vidas.
 
                     numBatalla++;
@@ -206,12 +223,34 @@ public class MapaActivity extends MiActivityPersonalizado
                 ObjectAnimator oa3 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_X, 530f);
 
                 as.playSequentially(oa1, oa2, oa3);
-
                 break;
-            case 3: break;
-            case 4: break;
-            case 5: break;
-            case 6: break;
+            case 3:
+                ObjectAnimator oa4 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_Y, -150f);
+                ObjectAnimator oa5 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_X, -450f);
+                ObjectAnimator oa6 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_Y, -200f);
+
+                as.playSequentially(oa4, oa5, oa6);
+                break;
+            case 4:
+                ObjectAnimator oa7 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_X, 450f);
+                ObjectAnimator oa8 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_Y, -450f);
+
+                as.playSequentially(oa7, oa8);
+                break;
+
+            case 5:
+                ObjectAnimator oa9 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_X, -200f);
+                ObjectAnimator oa10 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_Y, -50f);
+                ObjectAnimator oa11 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_X, -450f);
+
+                as.playSequentially(oa9, oa10, oa11);
+                break;
+            case 6:
+                ObjectAnimator oa12 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_Y, -150f);
+                ObjectAnimator oa13 = ObjectAnimator.ofFloat(viewPersonaje, View.TRANSLATION_X, 150f);
+
+                as.playSequentially(oa12, oa13);
+                break;
         }
 
         as.setDuration(1200);
@@ -230,5 +269,57 @@ public class MapaActivity extends MiActivityPersonalizado
         enemigos.add(new Enemigo(R.drawable.enemigo_residuos, "Residuos"));
 
         return enemigos;
+    }
+
+    private ArrayList<Pregunta> cargarPreguntas()
+    {
+        ArrayList<Pregunta> preguntas = null;
+
+        Locale lang = this.getResources().getConfiguration().locale;
+
+        String rutaFicheroJson = this.getFilesDir() + File.separator + "Preguntas" + File.separator;
+
+        switch (lang.getLanguage())
+        {
+            case "es": rutaFicheroJson += "preguntas_es.json"; break;
+            case "ca": rutaFicheroJson += "preguntas_ca.json"; break;
+            case "en": rutaFicheroJson += "preguntas_en.json"; break;
+            default:   rutaFicheroJson  = null;                break;
+        }
+
+        if (rutaFicheroJson != null)
+        {
+            BufferedReader br = null;
+
+            try
+            {
+                br = new BufferedReader(new FileReader(rutaFicheroJson));
+
+                preguntas = new ArrayList<>(Arrays.asList(new Gson().fromJson(br, Pregunta[].class)));
+
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                System.err.println("Fichero json no encontrado:\n" + ex.getMessage());
+            }
+            finally //Si ha saltado un error o no, cerraremos igualmente el BufferedReader.
+            {
+                try
+                {
+                    if (br != null) br.close();
+                }
+                catch (IOException ex)
+                {
+                    System.err.println("Error al cerrar el Buffered Reader:\n" + ex.getMessage());
+                }
+            }
+        }
+        else
+        {
+            System.err.println("No se ha podiddo obtener la ruta del fichero json. Idioma detectado: " + lang.getLanguage());
+        }
+
+        return preguntas;
     }
 }
