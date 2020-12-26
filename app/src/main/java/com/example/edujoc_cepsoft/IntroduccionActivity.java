4f299d947e7540b161105741ad93b490f2720ed0
Xiaolin Jin Lin <x140l1n@hotmail.com>
2020-12-26 23:14:01 +0100

    Resolución de errores.

--- app/src/main/java/com/example/edujoc_cepsoft/IntroduccionActivity.java
+++ app/src/main/java/com/example/edujoc_cepsoft/IntroduccionActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class introduction extends AppCompatActivity {
+public class IntroduccionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_introduction);
+        setContentView(R.layout.activity_introduccion);
     }
 }
